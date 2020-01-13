74e933916013d878d7bde46c2307a5913ba49ffe
Tony <tdevere@microsoft.com>
2020-01-13 16:06:42 -0600

    Add project files.

---
+++ AppCenter_WinForm/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AppCenter_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSendEvent = new System.Windows.Forms.Button();
+            this.txtSendEvent = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSendEvent
+            // 
+            this.btnSendEvent.Location = new System.Drawing.Point(12, 53);
+            this.btnSendEvent.Name = "btnSendEvent";
+            this.btnSendEvent.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnSendEvent.Size = new System.Drawing.Size(192, 23);
+            this.btnSendEvent.TabIndex = 0;
+            this.btnSendEvent.Text = "Send Event";
+            this.btnSendEvent.UseVisualStyleBackColor = true;
+            this.btnSendEvent.Click += new System.EventHandler(this.btnSendEvent_Click);
+            // 
+            // txtSendEvent
+            // 
+            this.txtSendEvent.Location = new System.Drawing.Point(210, 54);
+            this.txtSendEvent.Name = "txtSendEvent";
+            this.txtSendEvent.Size = new System.Drawing.Size(626, 22);
+            this.txtSendEvent.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 133);
+            this.Controls.Add(this.txtSendEvent);
+            this.Controls.Add(this.btnSendEvent);
+            this.Name = "Form1";
+            this.Text = "VS App Center Windows Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSendEvent;
+        private System.Windows.Forms.TextBox txtSendEvent;
+    }
+}
+
---
+++ AppCenter_WinForm/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace AppCenter_WinForm
+{
+    public partial class Form1 : Form
+    {
+        private const string app_key = "bdd9dcd9-5137-4cb8-9186-3f7f7a3c756a";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //AppCenter.LogLevel = LogLevel.Verbose;
+            AppCenter.Start(app_key, typeof(Analytics), typeof(Crashes));
+        }
+
+        private void btnSendEvent_Click(object sender, EventArgs e)
+        {
+            Analytics.TrackEvent($"DATE: {DateTime.Now.ToLongDateString()} Message: {txtSendEvent.Text.Trim()}");
+        }
+    }
+}
